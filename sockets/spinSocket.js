8b8c35a8940b6d5b570ada689b0e63c4ccc4edf8
Esqi-Meazza <sqmznapwi@gmail.com>
2026-08-23 22:13:29 +0700

    refactor: remove stale code cz trigger socket via controller, keep file strictly for connection handling

--- sockets/spinSocket.js
+++ sockets/spinSocket.js
@@ -1,17 +1,6 @@
-const spinService = require("../services/spinService");
-
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("Klien terhubung:", socket.id);
-
-    socket.on("admin-trigger-spin", async (data) => {
-      try {
-        const result = await spinService.processSpin();
-        io.emit("spin-result", result);
-      } catch (error) {
-        socket.emit("spin-rejected", { message: error.message });
-      }
-    });
 
     socket.on("disconnect", () => {
       console.log("Klien terputus:", socket.id);
